3f5c3271da6b580e6e6e8c4af03f5b8671eff363
hervé de CHAVIGN <vevedh@gmail.com>
2026-02-01 20:52:59 -0400

    update

--- docs/.vitepress/theme/components/BrandTitle.vue
+++ docs/.vitepress/theme/components/BrandTitle.vue
@@ -8,7 +8,7 @@ const { isDark } = useData()
 <template>
   <div class="vp-brand">
     <img
-      :src="isDark ? './images/plume-dark.png' : './images/plume-light.png'"
+      :src="isDark ? 'https://vevedh.github.io/nuxt-feathers-zod/images/plume-dark.png' : 'https://vevedh.github.io/nuxt-feathers-zod/images/plume-light.png'"
       alt="nuxt-feathers-zod"
       class="vp-brand-logo"
     >
